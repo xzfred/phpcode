a34d0a1cca4b5393b2dc8f7077d08d9a0f6c9591
徐智 <fred@freddeMBP.lan>
2017-11-21 23:41:23 +0800

    add template src

--- src/index.rs
+++ src/index.rs
@@ -1,7 +1,12 @@
-use rocket::{self, Request};
+use rocket::{self, Request, Route, Data, Catcher, Error};
 use rocket::response::Redirect;
 use rocket_contrib::Template;
 use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+use rocket::response::NamedFile;
+use rocket::handler::Outcome;
+use rocket::http::Method::*;
+use rocket::http::{Status, RawStr};
 
 #[derive(Serialize)]
 struct TemplateContext {
@@ -9,12 +14,28 @@ struct TemplateContext {
     items: Vec<String>
 }
 
+fn forward(req: &Request, data: Data) -> Outcome<'static> {
+    //Outcome::forward(data)
+    let param = req.uri()
+        .as_str()
+        //.split_at(6)
+        //.1
+        ;
+
+    Outcome::from(req, RawStr::from_str(param).url_decode())
+}
+
+#[get("/<file..>", rank = 2)]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("www/").join(file)).ok()
+}
+
 #[get("/")]
 fn index() -> Redirect {
     Redirect::to("/hello/Unknown")
 }
 
-#[get("/hello/<name>")]
+#[get("/hello/<name>", rank = 1)]
 fn get(name: String) -> Template {
     let context = TemplateContext {
         name: name,
@@ -24,7 +45,7 @@ fn get(name: String) -> Template {
     Template::render("index", &context)
 }
 
-#[catch(404)]
+#[error(404)]
 fn not_found(req: &Request) -> Template {
     let mut map = HashMap::new();
     map.insert("path", req.uri().as_str());
@@ -32,8 +53,11 @@ fn not_found(req: &Request) -> Template {
 }
 
 pub fn rocket() -> rocket::Rocket {
+    //let always_forward = Route::ranked(1, Get, "/", forward);
     rocket::ignite()
-        .mount("/", routes![index, get])
+        .mount("/", routes![index, get, files])
+        //.mount("/", vec![always_forward])
+        //.mount("/hello", routes![get])
         .attach(Template::fairing())
-        .catch(catchers![not_found])
+        .catch(errors![not_found])
 }
